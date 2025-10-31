5e573a808a925e1e80cf48156ad733999dddb766
Ilya Pavlukhin <i.pavluhin@ya.ru>
2025-10-31 11:53:44 +0300

    fix(types): add module declarations for *.svg?react and *.svg

--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,12 @@
 declare module "*.module.css";
+
+declare module "*.svg?react" {
+  import * as React from 'react';
+  const Component: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default Component;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
