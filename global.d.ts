511b2940ca9aee1a18708044392a6b4ea145d3fb
Ilya Pavlukhin <i.pavluhin@ya.ru>
2025-10-31 11:37:20 +0300

    fix(ts): add CSS/SVG module declarations and typeRoots for IDE; resolve *.module.css types

---
+++ global.d.ts
@@ -0,0 +1,15 @@
+declare module *.module.css {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module *.svg?react {
+  import * as React from "react";
+  const Component: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default Component;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
